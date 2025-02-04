719436bb1507aa4ca23d7204b322e9bb3e924e4a
Aron John Bautista <701035287@GoTVDSB.ca>
2025-02-04 14:45:04 -0500

    finished

--- Topic 1 - Assignment/Program.cs
+++ Topic 1 - Assignment/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Aron John E. Bautista
+            Console.WriteLine("Hello, I'm Aron John or just call me AJ");
+            Console.WriteLine();
+            Console.WriteLine("Comments are used for notes, as like a bookmark or for credits");
+            Console.WriteLine();
+            Console.WriteLine("  A    BBBB ");
+            Console.WriteLine(" A A   B   B ");
+            Console.WriteLine("A   A  B   B ");
+            Console.WriteLine("AAAAA  BBBB ");
+            Console.WriteLine("A   A  B   B ");
+            Console.WriteLine("A   A  B   B");
+            Console.WriteLine("A   A  BBBB ");
+            Console.WriteLine();
+            // This is my two initials
         }
     }
 }
