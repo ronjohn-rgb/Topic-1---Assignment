07fe6748b8da08426ff6fef3fca54179f267876c
Aron John Bautista <701035287@GoTVDSB.ca>
2025-02-04 14:15:43 -0500

    Add project files.

---
+++ Topic 1 - Assignment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_1___Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
